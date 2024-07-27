068022e773075ae097f163b7cf524f457994b4b2
fernandodamasceno <damasceno.developer21@gmail.com>
2024-07-27 00:58:33 -0300

    feat: initial nativewind configuration

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import {View, Text} from "react-native";
+
+export default function Index() {
+    return (
+        <View className="flex-1 items-center justify-center">
+            <Text className="text-white">ain jacksonnnn</Text>
+            <Text className="bg-red-400">Add for JSX based on type does not work in React Native....</Text>
+        </View>
+    )
+}
