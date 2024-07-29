a310cb58b9d4fd48b0dca32a75d5c3f493be0d40
fernandodamasceno <damasceno.developer21@gmail.com>
2024-07-29 12:30:05 -0300

    feat: add input component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,20 @@
-import {View, Text} from "react-native";
+import {Image, Text, View} from "react-native";
+import React from "react";
+import {Input} from "@/components/input";
 
 export default function Index() {
     return (
         <View className="flex-1 items-center justify-center">
-            <Text className="text-white">ain jacksonnnn</Text>
-            <Text className="bg-red-400">Add for JSX based on type does not work in React Native....</Text>
+            <Image source={require("@/assets/logo.png")} className="h-8" resizeMode="contain"/>
+            <Text className="text-zinc-400 font-sacramento text-center text-4xl mt-1 py-2">Convide seus amigos e planeje a {"\n"}próxima viagem</Text>
+            
+            <View>
+                <Input>
+                    <Input.Field placeholder="ain jackson"  ></Input.Field>
+                </Input>
+            </View>
+            
         </View>
     )
 }
+
